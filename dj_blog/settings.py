d5aa15419d630c6f7fe864422d21562299b040fe
John Grubb <john.grubb@advantagemedia.com>
2016-03-18 15:50:46 -0400

    removing material admin

--- dj_blog/settings.py
+++ dj_blog/settings.py
@@ -36,8 +36,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
-    'material',
-    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
