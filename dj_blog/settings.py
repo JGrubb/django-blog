b7fcfac33eed19347c2d1b6b3f748bbf3f7fd6f9
John Grubb <grubb@platform.sh>
2016-08-19 15:29:33 -0400

    collectstatic is a confusing POS

--- dj_blog/settings.py
+++ dj_blog/settings.py
@@ -164,7 +164,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'collected'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
